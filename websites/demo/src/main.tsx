ceac528482db2ffc08a5135fec3c6031f16a41c2
ManirajTiwari <maniraj26102006@gmail.com>
2026-09-24 22:39:07 +0530

    commit

--- websites/demo/src/main.tsx
+++ websites/demo/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AuthModalProps from './App';
 
 const rootElement = document.getElementById('root');
 
@@ -10,6 +10,6 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <AuthModalProps />
   </React.StrictMode>
 );
